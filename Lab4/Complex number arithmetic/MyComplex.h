4b8e28cf8327c0230a504454af40d8378072f772
orrinblake96 <orrin.blake.2016@mumail.ie>
2017-11-01 12:13:31 +0000

    Create MyComplex.h
    Header file for MyComplex class

---
+++ Lab4/Complex number arithmetic/MyComplex.h
@@ -0,0 +1,16 @@
+class MyComplex {
+protected:
+    double real;
+    double image;
+public:
+    void setReal(double);
+    void setImage(double);
+    double getReal();
+    double getImage();
+    MyComplex();
+    MyComplex(double, double);
+    MyComplex Add(const MyComplex &z);
+    MyComplex Subtraction(const MyComplex &z);
+    MyComplex Multiply(const MyComplex &z);
+    void print();
+};
