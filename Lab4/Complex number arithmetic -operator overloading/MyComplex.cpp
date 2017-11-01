908b47b5aa8df2c2d90eb8e972b1ceebbeba49e8
orrinblake96 <orrin.blake.2016@mumail.ie>
2017-11-01 12:16:19 +0000

    Create MyComplex.cpp
    Code for MyComplex class.

---
+++ Lab4/Complex number arithmetic -operator overloading/MyComplex.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "MyComplex.h"
+
+using namespace std;
+
+//default constructor which initialises both a and b to be set equal to zero.
+MyComplex::MyComplex() {
+    real = 0;
+    image = 0;
+}
+
+//constructor function that enables an object of this
+// class to be initialised when it is declared.
+MyComplex::MyComplex(double a, double b) {
+    real = a;
+    image = b;
+}
+
+//Getter and Setter methods for both the real and imaginary components
+void MyComplex::setReal(double r) {
+    real = r;
+}
+
+void MyComplex::setImage(double i) {
+    image = i;
+}
+
+double MyComplex::getReal() {
+    return real;
+}
+
+double MyComplex::getImage() {
+    return image;
+}
+
+//Addition of two MyComplex numbers, change Add to new instance
+MyComplex MyComplex::operator+(const MyComplex &z){
+    double r = real + z.real;
+    double i = image + z.image;
+
+    return MyComplex(r, i);
+}
+
+//Subtraction of two MyComplex numbers, change subtraction to new instance
+MyComplex MyComplex::operator-(const MyComplex &z){
+    double r = real - z.real;
+    double i = image - z.image;
+    return MyComplex(r, i);
+}
+
+//Multiplication of two MyComplex numbers, change multiplication to new instance
+MyComplex MyComplex::operator*(const MyComplex &z){
+    double r = ((real)*(z.real)) - ((image)*(z.image));
+    double i = ((real)*(z.image)) + ((image)*(z.real));
+    return MyComplex(r, i);
+}
+
+//print function to output a complex number
+// to the screen in the following format: “2 + 4i”
+void MyComplex::print() {
+    cout << real << " + " << image << "i" << endl;
+}
+
